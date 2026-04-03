89f11c93af9b1bc328df859a68fa7123d7ef9d1f
Igor Alves <igor.h.alves.ads@gmail.com>
2026-04-03 12:46:15 -0300

    Adicionado lombock

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity // Transofmra uma classe em uma entidade do banco de dados
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 // JPA = Java Persistence APÍ
 public class NinjaModel {
 
@@ -22,35 +28,3 @@ public class NinjaModel {
     @JoinColumn(name="missoes_id") // Foreing Key
     private MissoesModel missoesModel;
 
-    public NinjaModel() {
-    }
-    public NinjaModel(String nome, String email, Integer idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Integer idade) {
-        this.idade = idade;
-    }
-}
